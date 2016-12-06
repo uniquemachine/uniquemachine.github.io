c6770cd1149b29e0910a9232bd69cdc065fc7058
Song Li <993793072@qq.com>
2016-12-06 11:19:05 -0500

    new

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-cp -rf ./* /home/sol315/projs/uniquemachine.github.io/;
-cd /home/sol315/projs/uniquemachine.github.io/ ;
+cp -rf ./* ~/projs/uniquemachine.github.io/;
+cd ~/projs/uniquemachine.github.io/ ;
 git add -A;
 git commit -m "new";
 git push -u origin master;
