b6419d7d1047c6b33175b6cc082650596a5532f7
Song Li <993793072@qq.com>
2016-12-06 13:19:52 -0500

    new

--- js/main.js
+++ js/main.js
@@ -38,10 +38,12 @@ jQuery(function($) {
     */
 	});
 
+  /*
 	// Navigation Scroll
 	$(window).scroll(function(event) {
 		Scroll();
 	});
+  */
 
 	$('.navbar-collapse ul li a').on('click', function() {  
 		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
